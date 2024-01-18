8115b02e7b36578e7092b9991c84c6099a1d8e55
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-01-18 19:06:25 +0530

    fixed wrong block generation

---
+++ README.md
@@ -0,0 +1,5 @@
+install raylib from https://www.raylib.com/
+
+run the following command to build the game
+
+`g++ -I <path to raylib include  file> -L <path to raylib lib file> -o main main.cpp -lraylib -lgdi32 -lopengl32 -lwinmm`
--- main.cpp
+++ main.cpp
@@ -24,7 +24,8 @@ int PLAYER_x = WIDTH / 2;
 int PLAYER_y = 0;
 int MAX_ALTITUDE = 0;
 
-int main() {
+int main()
+{
     InitWindow(1280, 720, "Hello world");
     SetTargetFPS(60);
 
@@ -35,55 +36,69 @@ int main() {
     Texture2D *curr = &TEXTURE_I_MAN_L;
 
     int prev = 580;
-    for (int i = 0; i < 90; i++) {
+    for (int i = 0; i < 90; i++)
+    {
         prev += 180 - ((rand() * 5) % 365);
-        if(prev > 1190)
-            prev = (prev - 1190) % 95;
-        if (prev < 0)
-            prev = ((360 - prev) % 95) + 1100;
+        if (prev > 1160)
+        {
+            prev = 1160 - prev;
+        }
+        else if (prev < 60)
+        {
+            prev = 60 - prev;
+        }
         MAP[i] = prev;
     }
 
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose())
+    {
         PLAYER_y += speedy;
         int8_t y = PLAYER_y % 90;
         Y = PLAYER_y / 90;
         BLOCK_ABOVE = PLAYER_x > MAP[Y] - 30 && PLAYER_x < MAP[Y] + 90 && y > 30;
         BLOCK_BELOW = PLAYER_x > MAP[Y - 1] - 30 && PLAYER_x < MAP[Y - 1] + 90;
 
-        if (IsKeyDown(KEY_UP) && !FALLING) {
+        if (IsKeyDown(KEY_UP) && !FALLING)
+        {
             speedy = 15;
             FALLING = true;
         }
-        if (IsKeyDown(KEY_LEFT) && !(FALLING && (PLAYER_x > MAP[Y] - 30 && PLAYER_x < MAP[Y] + 95 && y > 30))) {
+        if (IsKeyDown(KEY_LEFT) && !(FALLING && (PLAYER_x > MAP[Y] - 30 && PLAYER_x < MAP[Y] + 95 && y > 30)))
+        {
             PLAYER_x = PLAYER_x - 5;
             if (PLAYER_x < 0)
                 PLAYER_x += 1245;
             curr = &TEXTURE_I_MAN_L;
         }
-        if (IsKeyDown(KEY_RIGHT) && !(FALLING && (PLAYER_x > MAP[Y] - 35 && PLAYER_x < MAP[Y] + 90 && y > 30))) {
+        if (IsKeyDown(KEY_RIGHT) && !(FALLING && (PLAYER_x > MAP[Y] - 35 && PLAYER_x < MAP[Y] + 90 && y > 30)))
+        {
             PLAYER_x = (PLAYER_x + 5) % 1255;
             curr = &TEXTURE_I_MAN_R;
         }
 
-        if (PLAYER_y < 0) {
+        if (PLAYER_y < 0)
+        {
             FALLING = false;
             speedy = 0;
             PLAYER_y = 0;
         }
-        else if (FALLING) {
+        else if (FALLING)
+        {
             speedy = (speedy <= -10) ? speedy : speedy - 1;
 
-            if (speedy > 0 && BLOCK_ABOVE) {
+            if (speedy > 0 && BLOCK_ABOVE)
+            {
                 speedy = -10;
                 PLAYER_y += speedy;
             }
-            else if (speedy < 0 && BLOCK_ABOVE) {
+            else if (speedy < 0 && BLOCK_ABOVE)
+            {
                 speedy = 0;
                 PLAYER_y = Y * 90 + 90;
                 FALLING = false;
-                if(Y+1 > MAX_ALTITUDE) {
-                    MAX_ALTITUDE = Y+1;
+                if (Y + 1 > MAX_ALTITUDE)
+                {
+                    MAX_ALTITUDE = Y + 1;
                 }
             }
         }
@@ -91,24 +106,26 @@ int main() {
             FALLING = true;
 
         BeginDrawing();
-            ClearBackground(black);
-            DrawTexture(*curr, PLAYER_x, HEIGHT - GROUND_Y, white);
-            DrawTexture(TEXTURE_GROUND, GROUND_X, HEIGHT - GROUND_Y + PLAYER_y + 4, white);
+        ClearBackground(black);
+        DrawTexture(*curr, PLAYER_x, HEIGHT - GROUND_Y, white);
+        DrawTexture(TEXTURE_GROUND, GROUND_X, HEIGHT - GROUND_Y + PLAYER_y + 4, white);
 
-            for (int j = (Y > 0) ? Y - 1 : 0; j < 90 && j <= Y + 7; j++) {
-                int x = MAP[j];
-                int y = HEIGHT - GROUND_Y - 60 - j * 90 + PLAYER_y;
-                DrawTexture(TEXTURE_BLOCKS, x, y, white);
-                DrawTexture(TEXTURE_BLOCKS, x + 30, y, white);
-                DrawTexture(TEXTURE_BLOCKS, x + 60, y, white);
-                // DrawText((std::to_string(x) + ", " + std::to_string(j*90 + 60)).c_str(), x, y-20, 8, white);
-            }
+        for (int j = (Y > 0) ? Y - 1 : 0; j < 90 && j <= Y + 7; j++)
+        {
+            int x = MAP[j];
+            int y = HEIGHT - GROUND_Y - 60 - j * 90 + PLAYER_y;
+            DrawTexture(TEXTURE_BLOCKS, x, y, white);
+            DrawTexture(TEXTURE_BLOCKS, x + 30, y, white);
+            DrawTexture(TEXTURE_BLOCKS, x + 60, y, white);
+            // DrawText((std::to_string(x) + ", " + std::to_string(j*90 + 60)).c_str(), x, y-20, 8, white);
+        }
 
-            // DrawText((std::to_string(PLAYER_x) + ", " + (std::to_string(y))).c_str(), PLAYER_x, HEIGHT - GROUND_Y - 20, 8, white);
-            DrawText(("score: " + std::to_string(MAX_ALTITUDE)).c_str(), 10, 10, 48, white);
-            if (MAX_ALTITUDE >= 90) {
-                DrawText("you won", 100, 200, 128, white);
-            }
+        // DrawText((std::to_string(PLAYER_x) + ", " + (std::to_string(y))).c_str(), PLAYER_x, HEIGHT - GROUND_Y - 20, 8, white);
+        DrawText(("score: " + std::to_string(MAX_ALTITUDE)).c_str(), 10, 10, 48, white);
+        if (MAX_ALTITUDE >= 90)
+        {
+            DrawText("you won", 100, 200, 128, white);
+        }
         EndDrawing();
     }
 
